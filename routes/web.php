13a16dec46c6c7a4dec54098a42356a77736aa33
Justin Dee <idlemuse@ghostlit.co.uk>
2018-09-07 23:30:42 +0100

    Basic restyling

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ Auth::routes();
 
 Route::middleware(['auth'])->group(function(){
 
-    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/', 'SiteNavigationController@index')->name('home');
 
 });
