d0d1c3d3966b4d24641470e748a4115fa849cd0a
Justin Dee <idlemuse@ghostlit.co.uk>
2018-09-22 15:56:01 +0100

    Messing around with fonts

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -8,10 +8,23 @@
         <a href="{{route('characters.edit', $character)}}" class="btn btn-sm btn-outline-secondary">Edit Character</a>
     </div>
     <div class="row">
-        <div class="col-5">
+        <div class="col-12">
             <h2 class="h3 mb-4">Details</h2>
         </div>
-        <div class="col-5 offset-1">
+        <div class="col-3">
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Player</label>
+                <div class="col-sm-9">
+                    {{$character->user->name}}
+                </div>
+            </div>
+        </div>
+        <div class="col-3 offset-1">
+        </div>
+        <div class="col-3 offset-1">
+        </div>
+        <div class="col-12">
+
         </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,6 @@
 
     <title>{{config('app.name','Laravel')}}</title>
 
-    <!-- Scripts -->
-    <script src="{{asset('js/app.js')}}" defer></script>
-
     <!-- Styles -->
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
@@ -48,13 +45,16 @@
         @endguest
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    @stack('scripts')
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> --}}
+
+    <!-- Scripts -->
+    <script src="{{asset('js/app.js')}}"></script>
     <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
     <script>
         feather.replace()
     </script>
+    @stack('scripts')
 </body>
 </html>
