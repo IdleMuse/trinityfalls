2ec6bfd88335e46900200e44fe6b398cd7c550fa
Justin Dee <idlemuse@ghostlit.co.uk>
2018-09-22 16:24:29 +0100

    show character screen complete

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -5,26 +5,109 @@
         <h1>
             {{$character->name}}
         </h1>
-        <a href="{{route('characters.edit', $character)}}" class="btn btn-sm btn-outline-secondary">Edit Character</a>
+        @can('update', $character)
+            <a href="{{route('characters.edit', $character)}}" class="btn btn-sm btn-outline-secondary">Edit Character</a>
+        @endcan
     </div>
-    <div class="row">
+    <div class="row border-bottom mx-0 mb-3 pb-3">
         <div class="col-12">
             <h2 class="h3 mb-4">Details</h2>
         </div>
-        <div class="col-3">
+        <div class="col-4">
             <div class="form-group row">
                 <label class="col-sm-3 col-form-label">Player</label>
-                <div class="col-sm-9">
-                    {{$character->user->name}}
+                <div class="col-sm-9 col-form-label font-weight-bold">
+                    <a href="{{route('users.edit',$character->user)}}">{{$character->user->name}}</a>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Status</label>
+                <div class="col-sm-9 col-form-label font-weight-bold">
+                    {{ucfirst($character->status)}}
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">XP</label>
+                <div class="col-sm-9 col-form-label font-weight-bold">
+                    (Work in Progress)
                 </div>
             </div>
         </div>
-        <div class="col-3 offset-1">
+        <div class="col-4">
+            <div class="form-group row">
+                <label class="col-sm-4 col-form-label">Personal Health Pool</label>
+                <div class="col-sm-8 col-form-label font-weight-bold">
+                    (Work in Progress)
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-4 col-form-label">Heroic Hit Points</label>
+                <div class="col-sm-8 col-form-label font-weight-bold">
+                    (Work in Progress)
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-4 col-form-label">Biofocus</label>
+                <div class="col-sm-8 col-form-label font-weight-bold">
+                    (Work in Progress)
+                </div>
+            </div>
         </div>
-        <div class="col-3 offset-1">
+        <div class="col-4">
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Age</label>
+                <div class="col-sm-9 col-form-label font-weight-bold">
+                    {{$character->age}}
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Wave</label>
+                <div class="col-sm-9 col-form-label font-weight-bold">
+                    {{ucwords($character->wave)}}
+                </div>
+            </div>
         </div>
+    </div>
+    @if(Auth::user()->is_admin)
+        <div class="row border-bottom mx-0 mb-3 pb-3">
+            <div class="col-12">
+                <div class="form-group">
+                    <label class="text-danger">Ref Notes</label>
+                    <p class="font-weight-bold">
+                        {{nl2br($character->ref_notes)}}
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endif
+    <div class="row border-bottom mx-0 mb-3 pb-3">
         <div class="col-12">
-
+            <div class="form-group">
+                <label>Description</label>
+                <p class="font-weight-bold">
+                    {{nl2br($character->description)}}
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row border-bottom mx-0 mb-3 pb-3">
+        <div class="col-12">
+            <div class="form-group">
+                <label>Background</label>
+                <p class="font-weight-bold">
+                    {{nl2br($character->background)}}
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row border-bottom mx-0 mb-3 pb-3">
+        <div class="col-12">
+            <div class="form-group">
+                <label>Major Incursion Events witnessed</label>
+                <p class="font-weight-bold">
+                    {{nl2br($character->mies)}}
+                </p>
+            </div>
         </div>
     </div>
 @endsection
