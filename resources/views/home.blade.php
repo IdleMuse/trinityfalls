bec651b798d1d6a779b409e5b1367cc59294f21b
Justin Dee <idlemuse@ghostlit.co.uk>
2018-09-23 12:34:10 +0100

    TIny refresh of the dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+    <div class="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pb-2 border-bottom mb-3">
+        <h1 class="display-4">Dashboard</h1>
     </div>
-</div>
+    {{-- <div class="row">
+        <div class="col-12">
+            You are logged in!
+        </div>
+    </div> --}}
+    <p class="lead text-center">
+        You are logged in!
+    </p>
+    @if(!Auth::user()->is_admin && empty(Auth::user()->active_character))
+        <p class="lead text-center">
+            You don't have an active character. Create one from <a href="{{route('users.edit', Auth::user())}}">your user page</a>.
+        </p>
+    @endif
 @endsection
